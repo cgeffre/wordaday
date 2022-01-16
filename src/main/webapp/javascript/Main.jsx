60b684c50a731391d016f9755e5f285491b52c47
Starr <starrleer@gmail.com>
2022-01-16 11:33:05 -0500

    lets see if i can get gradle to run...

--- src/main/webapp/javascript/Main.jsx
+++ src/main/webapp/javascript/Main.jsx
@@ -6,8 +6,32 @@ class Main extends Component {
     render() {
         return (
             <div id="main">
-                <h1>Demo Component</h1>
-                <img src="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"/>
+                <!--Create menu-->
+                <a href="#" class="logo">Portfolio</a>
+
+                <ul class="menu">
+                    <li> <a href="#home">Home</a> </li>
+                    <li> <a href="#study">Study</a> </li>
+                    <li> <a href="#Account">Account</a></li>
+                </ul>
+
+                <!--Top section starts-->
+                <section class="top" id="home">
+                    <div class="textBx">
+                        <h1>Word of the Day</h1>
+
+                        <p>Date/Time: <span id="datetime"></span></p>
+                        <script>
+                            var dt = new Date();
+                            document.getElementById("datetime").innerHTML = dt.toLocaleString();
+                        </script>
+                    </div>
+                </section>
+
+                <!--Favorite section-->
+
+                <!--Study button sections-->
+
             </div>
         );
     }
