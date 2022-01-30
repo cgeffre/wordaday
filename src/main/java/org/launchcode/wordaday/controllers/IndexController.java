08eedb4e4f283dcea429a9225d60da429310b8fe
cgeffre <cgeffre@gmail.com>
2022-01-30 14:48:57 -0500

    word api now calling definition api

--- src/main/java/org/launchcode/wordaday/controllers/IndexController.java
+++ src/main/java/org/launchcode/wordaday/controllers/IndexController.java
@@ -1,5 +1,6 @@
 package org.launchcode.wordaday.controllers;
 
+import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.launchcode.wordaday.models.dto.WordApiDTO;
 import org.springframework.stereotype.Controller;
@@ -19,8 +20,14 @@ public class IndexController {
         URL jsonWordUrl = new URL("http://api.wordnik.com/v4/words.json/randomWord?api_key=le18ljtuvicne235szv9snumadds2xyvy4xfbs82tymfkbn66");
         ObjectMapper mapper = new ObjectMapper();
         WordApiDTO wordApiDTO = mapper.readValue(jsonWordUrl, WordApiDTO.class);
-        model.addAttribute("wordApi", wordApiDTO.getWord());
         URL jsonDefURL = new URL("https://api.wordnik.com/v4/word.json/" + wordApiDTO.getWord() + "/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=le18ljtuvicne235szv9snumadds2xyvy4xfbs82tymfkbn66");
+        JsonNode array = mapper.readValue(jsonDefURL, JsonNode.class);
+        JsonNode object = array.get(0);
+        String defText = object.get("text").textValue();
+        String defSource = object.get("attributionText").textValue();
+        model.addAttribute("wordApi", wordApiDTO.getWord());
+        model.addAttribute("defText", defText);
+        model.addAttribute("defSource", defSource);
 
         return "index";
     }
