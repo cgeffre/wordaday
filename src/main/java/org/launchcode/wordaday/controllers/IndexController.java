c8253c0d24698ecdf3bc9068036bbc9cb4e5eccc
cgeffre <cgeffre@gmail.com>
2022-01-30 13:36:08 -0500

    added bootstrap and basic landing page

--- src/main/java/org/launchcode/wordaday/controllers/IndexController.java
+++ src/main/java/org/launchcode/wordaday/controllers/IndexController.java
@@ -16,10 +16,12 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) throws IOException {
-        URL jsonUrl = new URL("http://api.wordnik.com/v4/words.json/randomWord?api_key=le18ljtuvicne235szv9snumadds2xyvy4xfbs82tymfkbn66");
+        URL jsonWordUrl = new URL("http://api.wordnik.com/v4/words.json/randomWord?api_key=le18ljtuvicne235szv9snumadds2xyvy4xfbs82tymfkbn66");
         ObjectMapper mapper = new ObjectMapper();
-        WordApiDTO wordApiDTO = mapper.readValue(jsonUrl, WordApiDTO.class);
+        WordApiDTO wordApiDTO = mapper.readValue(jsonWordUrl, WordApiDTO.class);
         model.addAttribute("wordApi", wordApiDTO.getWord());
+        URL jsonDefURL = new URL("https://api.wordnik.com/v4/word.json/" + wordApiDTO.getWord() + "/definitions?limit=200&includeRelated=false&useCanonical=false&includeTags=false&api_key=le18ljtuvicne235szv9snumadds2xyvy4xfbs82tymfkbn66");
+
         return "index";
     }
 
