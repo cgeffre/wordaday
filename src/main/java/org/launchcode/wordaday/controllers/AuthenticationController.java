f65be1ce58941943c74f8da62bffa808d1521b18
cgeffre <cgeffre@gmail.com>
2022-02-02 11:55:02 -0500

    added logged in user landing page that personally greets by username

--- src/main/java/org/launchcode/wordaday/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/wordaday/controllers/AuthenticationController.java
@@ -81,7 +81,7 @@ public class AuthenticationController {
         userRepository.save(newUser);
         setUserInSession(request.getSession(), newUser);
 
-        return "redirect:";
+        return "redirect:/user";
     }
 
     @GetMapping("/login")
@@ -119,7 +119,7 @@ public class AuthenticationController {
 
         setUserInSession(request.getSession(), theUser);
 
-        return "redirect:";
+        return "redirect:/user";
     }
 
     @GetMapping("/logout")
--- src/main/java/org/launchcode/wordaday/controllers/IndexController.java
+++ src/main/java/org/launchcode/wordaday/controllers/IndexController.java
@@ -2,27 +2,32 @@ package org.launchcode.wordaday.controllers;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.launchcode.wordaday.models.User;
+import org.launchcode.wordaday.models.data.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.io.IOException;
+import javax.servlet.http.HttpSession;
 import java.net.URL;
 
 @Controller
 @RequestMapping
 public class IndexController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/")
-    public String index(Model model) throws IOException {
+    public String index(Model model) {
         try {
             URL jsonWordUrl = new URL("https://random-word-api.herokuapp.com/word?number=1");
             ObjectMapper mapper = new ObjectMapper();
             JsonNode wordArray = mapper.readValue(jsonWordUrl, JsonNode.class);
-            JsonNode wordObject = wordArray.get(0);
-            String wordText = wordObject.textValue();
-            URL jsonDefURL = new URL("https://dictionaryapi.com/api/v3/references/collegiate/json/" + wordObject + "?key=b5117ef5-0b38-4857-911e-f6c32b7a4eb1");
+            String wordText = wordArray.get(0).textValue();
+            URL jsonDefURL = new URL("https://dictionaryapi.com/api/v3/references/collegiate/json/" + wordText + "?key=b5117ef5-0b38-4857-911e-f6c32b7a4eb1");
             JsonNode defApi = mapper.readValue(jsonDefURL, JsonNode.class);
             String defText = defApi.get(0).get("shortdef").get(0).textValue();
             if (wordText != null && defText != null) {
@@ -36,12 +41,39 @@ public class IndexController {
             return "index";
         }
         catch (Exception e) {
-
-        }
-        finally {
             model.addAttribute("wordApi", "Oops! We encountered an error. Please wait a few moments and then try again.");
             model.addAttribute("defText", "");
             return "index";
+        }
+    }
+
+    @GetMapping("/user")
+    public String loggedInIndex(Model model, HttpSession session) {
+        String userSessionKey = "user";
+        Integer userId = (Integer) session.getAttribute(userSessionKey);
+        User user = userRepository.findById(userId).orElse(new User());
+        model.addAttribute("username", user.getUsername());
+        try {
+            URL jsonWordUrl = new URL("https://random-word-api.herokuapp.com/word?number=1");
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode wordArray = mapper.readValue(jsonWordUrl, JsonNode.class);
+            String wordText = wordArray.get(0).textValue();
+            URL jsonDefURL = new URL("https://dictionaryapi.com/api/v3/references/collegiate/json/" + wordText + "?key=b5117ef5-0b38-4857-911e-f6c32b7a4eb1");
+            JsonNode defApi = mapper.readValue(jsonDefURL, JsonNode.class);
+            String defText = defApi.get(0).get("shortdef").get(0).textValue();
+            if (wordText != null && defText != null) {
+                model.addAttribute("wordApi", wordText);
+                model.addAttribute("defText", defText);
+            }
+            if (wordText == null || defText == null) {
+                model.addAttribute("wordApi", "Oops! We encountered an error. Please wait a moment and then try again.");
+                model.addAttribute("defText", "");
+            }
+            return "/user/index";
+        } catch (Exception e) {
+            model.addAttribute("wordApi", "Oops! We encountered an error. Please wait a few moments and then try again.");
+            model.addAttribute("defText", "");
+            return "/user/index";
         }
     }
 }
