35b64aa9107249e228c228546aff02531018d7d1
cgeffre <cgeffre@gmail.com>
2022-02-02 10:53:41 -0500

    added error handling for failed API calls and null values for word or definition

--- src/main/java/org/launchcode/wordaday/controllers/IndexController.java
+++ src/main/java/org/launchcode/wordaday/controllers/IndexController.java
@@ -16,15 +16,32 @@ public class IndexController {
 
     @GetMapping("/")
     public String index(Model model) throws IOException {
-        URL jsonWordUrl = new URL("https://random-word-api.herokuapp.com/word?number=1");
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode wordArray = mapper.readValue(jsonWordUrl, JsonNode.class);
-        JsonNode wordObject = wordArray.get(0);
-        String wordText = wordObject.textValue();
-        URL jsonDefURL = new URL("https://dictionaryapi.com/api/v3/references/collegiate/json/" + wordObject + "?key=b5117ef5-0b38-4857-911e-f6c32b7a4eb1");
-        JsonNode defApi = mapper.readValue(jsonDefURL, JsonNode.class);
-        String defText1 = defApi.get(0).get("shortdef").get(0).textValue();
-        return "index";
-    }
+        try {
+            URL jsonWordUrl = new URL("https://random-word-api.herokuapp.com/word?number=1");
+            ObjectMapper mapper = new ObjectMapper();
+            JsonNode wordArray = mapper.readValue(jsonWordUrl, JsonNode.class);
+            JsonNode wordObject = wordArray.get(0);
+            String wordText = wordObject.textValue();
+            URL jsonDefURL = new URL("https://dictionaryapi.com/api/v3/references/collegiate/json/" + wordObject + "?key=b5117ef5-0b38-4857-911e-f6c32b7a4eb1");
+            JsonNode defApi = mapper.readValue(jsonDefURL, JsonNode.class);
+            String defText = defApi.get(0).get("shortdef").get(0).textValue();
+            if (wordText != null && defText != null) {
+                model.addAttribute("wordApi", wordText);
+                model.addAttribute("defText", defText);
+            }
+            if (wordText == null || defText == null) {
+                model.addAttribute("wordApi", "Oops! We encountered an error. Please wait a moment and then try again.");
+                model.addAttribute("defText", "");
+            }
+            return "index";
+        }
+        catch (Exception e) {
 
+        }
+        finally {
+            model.addAttribute("wordApi", "Oops! We encountered an error. Please wait a few moments and then try again.");
+            model.addAttribute("defText", "");
+            return "index";
+        }
+    }
 }
