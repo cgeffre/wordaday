e1f19c4bb15c6f9611837697e555e99e0bd824a4
cgeffre <cgeffre@gmail.com>
2022-02-02 14:43:24 -0500

    created recursive method for APIs that generates new word if definition API returns null

--- src/main/java/org/launchcode/wordaday/controllers/IndexController.java
+++ src/main/java/org/launchcode/wordaday/controllers/IndexController.java
@@ -3,7 +3,9 @@ package org.launchcode.wordaday.controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.launchcode.wordaday.models.User;
+import org.launchcode.wordaday.models.Word;
 import org.launchcode.wordaday.models.data.UserRepository;
+import org.launchcode.wordaday.models.data.WordRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -20,21 +22,19 @@ public class IndexController {
     @Autowired
     UserRepository userRepository;
 
+    @Autowired
+    WordRepository wordRepository;
+
     @GetMapping("/")
     public String index(Model model) {
         try {
-            URL jsonWordUrl = new URL("https://random-word-api.herokuapp.com/word?number=1");
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode wordArray = mapper.readValue(jsonWordUrl, JsonNode.class);
-            String wordText = wordArray.get(0).textValue();
-            URL jsonDefURL = new URL("https://dictionaryapi.com/api/v3/references/collegiate/json/" + wordText + "?key=b5117ef5-0b38-4857-911e-f6c32b7a4eb1");
-            JsonNode defApi = mapper.readValue(jsonDefURL, JsonNode.class);
-            String defText = defApi.get(0).get("shortdef").get(0).textValue();
-            if (wordText != null && defText != null) {
-                model.addAttribute("wordApi", wordText);
-                model.addAttribute("defText", defText);
+            Word word = new Word();
+            word.generateRandomFromApis(word);
+            if (word.getWord() != null && word.getDefinition() != null) {
+                model.addAttribute("wordApi", word.getWord());
+                model.addAttribute("defText", word.getDefinition());
             }
-            if (wordText == null || defText == null) {
+            if (word.getWord() == null || word.getDefinition() == null) {
                 model.addAttribute("wordApi", "Oops! We encountered an error. Please wait a moment and then try again.");
                 model.addAttribute("defText", "");
             }
@@ -54,23 +54,19 @@ public class IndexController {
         User user = userRepository.findById(userId).orElse(new User());
         model.addAttribute("username", user.getUsername());
         try {
-            URL jsonWordUrl = new URL("https://random-word-api.herokuapp.com/word?number=1");
-            ObjectMapper mapper = new ObjectMapper();
-            JsonNode wordArray = mapper.readValue(jsonWordUrl, JsonNode.class);
-            String wordText = wordArray.get(0).textValue();
-            URL jsonDefURL = new URL("https://dictionaryapi.com/api/v3/references/collegiate/json/" + wordText + "?key=b5117ef5-0b38-4857-911e-f6c32b7a4eb1");
-            JsonNode defApi = mapper.readValue(jsonDefURL, JsonNode.class);
-            String defText = defApi.get(0).get("shortdef").get(0).textValue();
-            if (wordText != null && defText != null) {
-                model.addAttribute("wordApi", wordText);
-                model.addAttribute("defText", defText);
+            Word word = new Word();
+            word.generateRandomFromApis(word);
+            if (word.getWord() != null && word.getDefinition() != null) {
+                model.addAttribute("wordApi", word.getWord());
+                model.addAttribute("defText", word.getDefinition());
             }
-            if (wordText == null || defText == null) {
+            if (word.getWord() == null || word.getDefinition() == null) {
                 model.addAttribute("wordApi", "Oops! We encountered an error. Please wait a moment and then try again.");
                 model.addAttribute("defText", "");
             }
             return "/user/index";
-        } catch (Exception e) {
+        }
+        catch (Exception e) {
             model.addAttribute("wordApi", "Oops! We encountered an error. Please wait a few moments and then try again.");
             model.addAttribute("defText", "");
             return "/user/index";
--- src/main/java/org/launchcode/wordaday/models/Word.java
+++ src/main/java/org/launchcode/wordaday/models/Word.java
@@ -1,8 +1,14 @@
 package org.launchcode.wordaday.models;
 
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import javax.persistence.Entity;
 import javax.persistence.ManyToMany;
 import javax.persistence.OneToMany;
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -12,17 +18,37 @@ public class Word extends AbstractEntity {
     private String word;
     private String definition;
 
-    @ManyToMany(mappedBy="words")
-    private Set<Deck> deck = new HashSet<>();
-
     public Word() {}
 
-    public Set<Deck> getDeck() {
-        return deck;
+    public Word generateRandomFromApis(Word word) throws IOException {
+        URL jsonWordUrl = new URL("https://random-word-api.herokuapp.com/word?number=1");
+        ObjectMapper mapper = new ObjectMapper();
+        JsonNode wordArray = mapper.readValue(jsonWordUrl, JsonNode.class);
+        String wordText = wordArray.get(0).textValue();
+        URL jsonDefURL = new URL("https://dictionaryapi.com/api/v3/references/collegiate/json/" + wordText + "?key=b5117ef5-0b38-4857-911e-f6c32b7a4eb1");
+        JsonNode defApi = mapper.readValue(jsonDefURL, JsonNode.class);
+        String defText = defApi.get(0).get("shortdef").get(0).textValue();
+        word.setWord(wordText);
+        word.setDefinition(defText);
+        if (word.getDefinition() == null) {
+            word.generateRandomFromApis(word);
+        }
+        return word;
     }
 
-    public void setDeck(Set<Deck> deck) {
-        this.deck = deck;
+    public String getWord() {
+        return word;
     }
 
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
 }
