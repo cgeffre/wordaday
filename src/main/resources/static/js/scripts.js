0c59290c51cb886cb3ba226f1e8b4657c0162712
cgeffre <cgeffre@gmail.com>
2022-02-13 13:38:15 -0500

    basic iteration through flashcards with right and wrong answers

--- src/main/resources/static/js/scripts.js
+++ src/main/resources/static/js/scripts.js
@@ -1,23 +1,27 @@
 // Iterates through word flashcards; displays and hides definitions; tracks correct and incorrect answers
 function flashcards(wordCount) {
     let counter = 0;
-    let correctAnswer = 0;
+    let rightAnswer = 0;
     let wrongAnswer = 0;
-//    let showDefinitions = document.getElementById("showDefinitions");
-//    const correctAnswer = document.getElementById("correctAnswer");
 
-    if (counter < wordCount) {
-        document.getElementById(counter-1).hidden = true;
-        document.getElementById(counter).hidden = false;
-    }
+    document.getElementById(counter).hidden = false;
 
-//    correctAnswer.addEventListener('click', function(event) {
-//        document.getElementById("definitions").hidden = true;
-//        counter++;
-//    });
+    document.getElementById("rightAnswer").addEventListener('click', function(event) {
+        document.getElementById(counter).hidden = true;
+        document.getElementById(counter+1).hidden = false;
+        correctAnswer++;
+        counter++;
+    });
 
-//    showDefinitions.addEventListener('click', function() {
-//        document.getElementById(definitions).hidden = false;
-//    });
+    document.getElementById("wrongAnswer").addEventListener('click', function(event) {
+        document.getElementById(counter).hidden = true;
+        document.getElementById(counter+1).hidden = false;
+        wrongAnswer++;
+        counter++;
+    });
+
+    document.getElementById("showDefinitions").addEventListener('click', function() {
+        document.getElementById("definitions").hidden = false;
+    });
 
 }
