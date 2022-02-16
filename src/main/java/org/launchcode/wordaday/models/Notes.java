08ae6269b5eb9abef00eadf4d627074af084f6a4
cgeffre <cgeffre@gmail.com>
2022-02-16 14:47:04 -0500

    added notes class and initial mapping for notes

---
+++ src/main/java/org/launchcode/wordaday/models/Notes.java
@@ -0,0 +1,33 @@
+package org.launchcode.wordaday.models;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+
+@Entity
+public class Notes extends AbstractEntity {
+
+    @OneToOne(mappedBy="notes")
+    private Word word;
+
+    @Column(length=1000)
+    private String notes;
+
+    public Notes(){}
+
+    public Word getWord() {
+        return word;
+    }
+
+    public void setWord(Word word) {
+        this.word = word;
+    }
+
+    public String getNotes() {
+        return notes;
+    }
+
+    public void setNotes(String notes) {
+        this.notes = notes;
+    }
+}
