247eddd63893b06b61ad34ef8f24c765a21d9e2c
cgeffre <cgeffre@gmail.com>
2022-02-23 11:57:39 -0500

    preparing for heroku deployment

--- src/main/java/org/launchcode/wordaday/WordadayApplication.java
+++ src/main/java/org/launchcode/wordaday/WordadayApplication.java
@@ -1,18 +1,17 @@
 package org.launchcode.wordaday;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-@Configuration
-@EnableAutoConfiguration
-@EnableJpaRepositories
-@ComponentScan
-public class WordadayApplication {
+public class WordadayApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(WordadayApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(WordadayApplication.class, args);
